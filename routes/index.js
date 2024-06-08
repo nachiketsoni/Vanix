5de15e9f61129591ed169421ec4abf6d80560570
nxchikxt <nxchikxt@gmail.com>
2024-06-08 18:55:28 +0530

    Final Commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const routes = express.Router();
 
+const index = require("./views/index.views.routes");
 const user = require("./user.routes");
 const auth = require("./auth.routes");
 const contact = require("./contact.routes");
 
 
+routes.use("/", index);
 routes.use("/user", user);
 routes.use("/auth", auth);
 routes.use("/contact", contact);
